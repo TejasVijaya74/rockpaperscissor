e4deaa8a0ae03e727975e52be4d9996bdc5e5518
TejasVijaya74 <vt6267700@gmail.com>
2024-02-09 07:35:04 +0530

    Add test module

---
+++ test_module.py
@@ -0,0 +1,4 @@
+# This will run the play function with the updated player function and the quincy opponent. 
+# The results of each game will be printed to the console.
+
+play(player, quincy, 1000, verbose=True)
